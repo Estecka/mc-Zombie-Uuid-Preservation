637a5bea83d2e13b9125839a9d64def482c26c1f
Estecka <estecka@galacsys.com>
2024-11-01 22:49:31 +0100

    Ported to MC 1.21.2

--- README.md
+++ README.md
@@ -3,10 +3,12 @@
 If you use texture-packs that randomize villager textures, you may have noticed that their appearances change after being zombified or cured. 
 This mod fixes that, by ensuring the villager's UUID is preserved after each conversion.
 
-**The fix occurs on the server-side**
+**The fix is made the server-side**
 
 Although being developped around villagers, this will also takes effect on some other forms of conversion:
 - Piglin and Hoglin zombification
 - Skeletons turning into strays
 - Husk turning into zombies
 - Zombies turning into drowned
+
+This *will not* affect slimes and the like splitting up, even though this is considered a "conversion" since MC 1.21.2.
--- src/main/java/fr/estecka/uuidpreserve/mixin/MobEntityMixin.java
+++ src/main/java/fr/estecka/uuidpreserve/mixin/MobEntityMixin.java
@@ -5,8 +5,10 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.conversion.EntityConversionContext;
+import net.minecraft.entity.conversion.EntityConversionContext.Finalizer;
 import net.minecraft.entity.mob.MobEntity;
 
 
@@ -15,21 +17,18 @@ public abstract class MobEntityMixin
 {
 	/**
 	 * Reimplements most of the tail end of the method, but performs the actions
-	 * in a different order. The current entity needs to be discarded before the
-	 * new one is spawned in order to avoid UUID conflicts.
+	 * in a different order. The old entity needs to be discarded before the new
+	 * one is spawned in order to avoid UUID conflicts.
 	 */
-	@Inject( method="convertTo", cancellable=true, at=@At(value="INVOKE", target="net/minecraft/world/World.spawnEntity(Lnet/minecraft/entity/Entity;)Z") )
-	private void PreserveUuid(EntityType<?> neoType, boolean keepEquipment, CallbackInfoReturnable<MobEntity> ci, @Local MobEntity neoEntity)
+	@Inject( method="convertTo", cancellable=true, at=@At(value="INVOKE", target="net/minecraft/server/world/ServerWorld.spawnEntity(Lnet/minecraft/entity/Entity;)Z") )
+	private void PreserveUuid(EntityType<?> neoType, EntityConversionContext context, SpawnReason reason, Finalizer<?> finalizer, CallbackInfoReturnable<MobEntity> ci, @Local MobEntity neoEntity)
 	{
-		MobEntity me = (MobEntity)(Object)this;
-		Entity vehicle = me.getVehicle();
-
-		me.discard();
-		neoEntity.setUuid(me.getUuid());
-		me.getWorld().spawnEntity(neoEntity);
-		if (vehicle != null)
-			neoEntity.startRiding(vehicle);
-
-		ci.setReturnValue(neoEntity);
+		if (context.type().shouldDiscardOldEntity()) {
+			MobEntity oldEntity = (MobEntity)(Object)this;
+			oldEntity.discard();
+			neoEntity.setUuid(oldEntity.getUuid());
+			oldEntity.getWorld().spawnEntity(neoEntity);
+			ci.setReturnValue(neoEntity);
+		}
 	}
 }
